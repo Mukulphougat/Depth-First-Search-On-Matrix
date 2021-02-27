42a5a85f7c6bd5afd22c4ecf59dc2471abad39ec
Mukul Phougat <mukulphoughat@gmail.com>
2021-02-27 14:39:47 +0530

    add

---
+++ src/NumberOfClosedIslands.java
@@ -0,0 +1,11 @@
+public class NumberOfClosedIslands {
+    public static int closedIslands(int[][] grid){
+        if ( grid == null || grid.length == 0 ) return 0;
+        int closedIsland = 0, row = grid.length , col = grid[0].length;
+        for ( int i = 1 ; i < row - 1 ; i++ ){
+            for ( int j = 1 ; j < col - 1 ; j++ ){
+
+            }
+        }
+    }
+}
